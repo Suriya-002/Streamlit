6c6b025241f133ff68247f1b147cfe9e28e379c6
Suriya R <suriyasparks007@gmail.com>
2023-03-31 22:03:53 +0530

    plotly addition

---
+++ plotly.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+
+def main():
+    st.title("Plotting in streamlit with plotly")
+    df = pd.read_csv("C://Users//SURIYA//Documents//StreamLit//streamlit//code//streamlit-practice//stacked.csv")
+    st.dataframe(df)
+
+    fig = px.pie(df, values="Sum", names="lang", title="Pie chart of languages")
+    st.plot(fig)
+
+
+
+if __name__ == "__main__":
+    main()
